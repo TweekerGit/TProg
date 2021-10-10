aa4403d8c647b08c63bc59ef837442991e24da9a
jurilents <keeeneeek@gmail.com>
2021-10-10 21:59:05 +0300

    Add many children generator

--- ElementarySchool/ChildrenFactory.cs
+++ ElementarySchool/ChildrenFactory.cs
@@ -10,10 +10,16 @@ namespace ElementarySchool
 		private readonly Random _rand = new();
 
 
-		public Child Random(double proportion = 0.5)
+		public IEnumerable<Child> RandomMany(int count, double proportion = 0.5)
 		{
-			return _rand.NextDouble() > proportion ? RandomMale() : RandomFemale();
+			if (count <= 0)
+				throw new ArgumentOutOfRangeException(nameof(count));
+
+			for (int i = 0; i < count; i++)
+				yield return Random(proportion);
 		}
+
+		public Child Random(double proportion = 0.5) => _rand.NextDouble() > proportion ? RandomMale() : RandomFemale();
 
 		public Child RandomMale()
 		{
@@ -38,13 +44,13 @@ namespace ElementarySchool
 		}
 
 
-		private static readonly string[] MaleNames = {"Іваня", "Кирюша", "Серьожа", "Нікітка", "Данило", "Денчік", "Стьопа", "Костя", "Лесь", "Джон", "Діо", "Димитро", "Ромчик", "Йосип", "Ерен"};
-		private static readonly string[] FemaleNames = {"Анічка", "Олена", "Люда", "Оля", "Маруся", "Діана", "Маргарита", "Вікторія", "Саша", "Хрестина", "Улянка", "Злата", "Ичіго", "Таня", "Мікаса"};
-		private static readonly string[] Surnames = {"Шевченко", "Черненко", "Шмідт", "Ульм", "Гуро Гуро", "Джон", "Попенко", "Романов", "Білочка", "Енштейн", "Ферштейн", "Геббельс", "Габсбург", "Червондель", "Купрій", "Мамамото", "Ніт", "Жмайло", "Тиран", "Загреб", "Мілано", "Драгон", "Нгуен", "Кок", "Орочі", "Євнух", "Більбо", "Ґудзик"};
+		private static readonly string[] MaleNames = {"Іваня", "Кирюша", "Серьожа", "Нікітка", "Данило", "Денчік", "Стьопа", "Юрій", "Владік", "Костя", "Лесь", "Джон", "Діо", "Димитро", "Ромчик", "Йосип", "Ерен"};
+		private static readonly string[] FemaleNames = {"Анічка", "Олена", "Люда", "Оля", "Маруся", "Діана", "Маргарита", "Юля", "Вікторія", "Саша", "Хрестина", "Улянка", "Злата", "Ичіго", "Таня", "Мікаса"};
+		private static readonly string[] Surnames = {"Шевченко", "Черненко", "Шмідт", "Ульм", "Гуро Гуро", "Джон", "Попенко", "Романов", "Білочка", "Енштейн", "Ферштейн", "Геббельс", "Габсбург", "Червондель", "Купрій", "Мамамото", "Ніт", "Жмайло", "Тиран", "Загреб", "Мілано", "Драгон", "Нгуен", "Кок", "Орочі", "Євнух", "Більбо", "Ґудзик", "Акерман", "Єгер", "Фіт", "Колбасенко", "Япончик"};
 
 		private string GenerateMaleName() => PickRandom(MaleNames) + " " + PickRandom(Surnames);
 		private string GenerateFemaleName() => PickRandom(FemaleNames) + " " + PickRandom(Surnames);
-		private byte GenerateAge() => (byte) _rand.Next(7, 13);
+		private byte GenerateAge() => (byte) _rand.Next(5, 16);
 
 		private T PickRandom<T>(IReadOnlyList<T> array) => array[_rand.Next(0, array.Count)];
 	}
