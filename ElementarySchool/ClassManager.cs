43244212d4c51791aaaeb04376de97bcee4a9e38
tweeker <programmeracc38@gmail.com>
2021-10-10 20:08:06 +0300

    Init Project

---
+++ ElementarySchool/ClassManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using ElementarySchool.Entities;
+
+namespace ElementarySchool
+{
+    public class ClassManager
+    {
+        private List<Child> _children;
+
+        public ClassManager(List<Child> children)
+        {
+            _children = GenerateKids();
+        }
+
+        private List<Child> GenerateKids()
+        {
+            return new List<Child>();
+        }
+    }
+}
---
+++ ElementarySchool/Entities/Child.cs
@@ -0,0 +1,10 @@
+﻿namespace ElementarySchool.Entities
+{
+    public class Child
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool? Sex { get; set; }
+        public byte Age { get; set; }
+    }
+}
